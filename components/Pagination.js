f538743da21d59d8460bde000f7bdd16182c6fd5
praqo <142553687+praqo@users.noreply.github.com>
2023-12-07 21:33:27 -0800

    add initial pagination styles

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,3 +1,33 @@
-export default function Pagination({ data }) {
-  return <h5>Pages {data.totalCount / 100}</h5>;
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useGlobalContext } from "../context/context";
+import Router, { useRouter } from "next/router";
+
+export default function Pagination({ pageQuery }) {
+  const router = useRouter();
+  const { searchData, searchResults, isLoading } = useGlobalContext();
+  const numerOfPages = Math.ceil(searchResults.totalCount / 100);
+
+  const handleClick = (page) => {
+    Router.push({
+      pathname: "/search",
+      query: {
+        q: pageQuery,
+        page,
+      },
+    });
+  };
+
+  return (
+    <h5>
+      Pages{" "}
+      {[...Array(numerOfPages)].map((e, i) => {
+        return (
+          <span key={i} onClick={() => handleClick(i + 1)}>
+            {i + 1}
+          </span>
+        );
+      })}
+    </h5>
+  );
 }
