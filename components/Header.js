f137e4073f615e1ced64bc69571032fb2319931f
praqo <142553687+praqo@users.noreply.github.com>
2023-12-11 21:33:27 -0800

    start header design

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,20 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <Link href="/">
-      <a>Library Mage</a>
-    </Link>
+    <header className="main-header">
+      <div className="logo-area">
+        <Link href="/">
+          <a className="logo-link">Library Mage</a>
+        </Link>
+      </div>
+      <nav className="header-nav">
+        <button className="header-search"></button>
+        <div className="header-menu">
+          <button className="menu-see"></button>
+          <button className="menu-burger"></button>
+        </div>
+      </nav>
+    </header>
   );
 }
 
