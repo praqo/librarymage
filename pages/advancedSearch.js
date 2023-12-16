9b2a21145e8db0cf20f63a7e9bb7babb34660377
praqo <142553687+praqo@users.noreply.github.com>
2023-12-15 21:33:27 -0800

    rename attributes into react format

--- pages/advancedSearch.js
+++ pages/advancedSearch.js
@@ -1,13 +1,6 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+
 
 function advancedSearch() {
-  const router = useRouter();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submit");
-  };
 
   return <h2>advanced search</h2>;
 }
